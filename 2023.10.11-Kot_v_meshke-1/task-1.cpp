4e9ea8b869969fbc7114e5e7f7334c045b04ab90
st117163 <144270893+st117163@users.noreply.github.com>
2023-10-11 20:06:38 +0300

    Task-1 for kot v meshke uploaded

---
+++ 2023.10.11-Kot_v_meshke-1/task-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+
+	std::cin >> a;
+	std::cin >> b;
+
+	int c = a + b;
+
+	std::cout << c << std::endl;
+	return EXIT_SUCCESS;
+}

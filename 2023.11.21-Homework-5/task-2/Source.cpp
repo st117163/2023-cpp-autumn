e6fb438f69a36f2842308652cc7f907efd0b3d4a
st117163 <st117163@student.spbu.ru>
2023-11-23 02:07:54 +0300

    extra files deleted

---
+++ 2023.11.21-Homework-5/task-2/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	int size = 0;
+
+	std::cin >> size;
+
+	int* arr = (int*)malloc(size * sizeof(int));
+
+	if (arr)  //to avoid Warning C6011
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			std::cin >> *(arr + i);
+		}
+		for (int i = 0; i < size; i += 2)
+		{
+			std::cout << *(arr + i) << ' ';
+		}
+		free(arr);
+	}
+	return EXIT_SUCCESS;
+}
